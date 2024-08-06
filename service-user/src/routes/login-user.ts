5ea8dc1e83705311b14761725a57d227a2e6e1b9
Juan Rey <sudo.delete.evil@gmail.com>
2024-08-06 11:41:17 -0500

    6-8-2024

--- service-user/src/routes/login-user.ts
+++ service-user/src/routes/login-user.ts
@@ -3,53 +3,105 @@ import { Request, Response } from "express";
 import { createToken } from "../utils/token";
 import { UserToken, userMiddleware } from "../utils/user";
 import { _ } from "../utils/pino";
+import { getUserId } from "../utils/nano";
+import { getPubKeyHash } from "../utils/crypto";
+import Cardano from "@emurgo/cardano-serialization-lib-nodejs";
 import DB from "../db";
+
+const verifyDataSignature = require("@cardano-foundation/cardano-verify-datasignature");
 
 const loginUserMiddlewares: any = [userMiddleware];
 
 const loginUserHandler = async (req: Request, res: Response) => {
   let connection = null;
   let params = req.body;
+
+  console.log(params);
+
   try {
-    if (params.userData) {
-      throw new Error("logged");
+    const address = Cardano.Address.from_hex(params.address);
+    const address32: string = address.to_bech32();
+    const pubkeyhash = getPubKeyHash(address);
+    const signature = params.signature;
+    const message = "PLEASE SIGN TO AUTHENTICATE IN PAIRFY";
+
+    const verifySignature = verifyDataSignature(
+      signature.signature,
+      signature.key,
+      message,
+      address32
+    );
+
+    if (!verifySignature) {
+      throw new BadRequestError("AUTH_FAILED");
     }
+
+    ///////////////////////////////////////////////////////
 
     connection = await DB.client.getConnection();
 
-    const [rows] = await connection.execute(
-      "SELECT * FROM users WHERE wallet = ?",
-      [params.wallet]
-    );
+    await connection.beginTransaction();
 
-    if (rows.length === 0) {
-      throw new Error("nonexist");
-    }
+    const userId = getUserId();
+    const username = "server";
+    const country = "server";
 
-    const USER = rows[0];
+    const schemeData = `
+    INSERT INTO users (
+      id,
+      username,
+      address,
+      pubkeyhash,
+      country,
+      terms_accepted,
+      public_ip,
+      schema_v
+     ) 
+     VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+     ON DUPLICATE KEY UPDATE
+      username = VALUES(username),
+      address = VALUES(address),
+      pubkeyhash = VALUES(pubkeyhash),
+      country = VALUES(country),
+      terms_accepted = VALUES(terms_accepted),
+      public_ip = VALUES(public_ip),
+      schema_v = VALUES(schema_v);
+     `;
 
+    const schemeValue = [
+      userId,
+      username,
+      address32,
+      pubkeyhash,
+      country,
+      params.terms_accepted,
+      "192.168.1.1",
+      0,
+    ];
 
     const userData: UserToken = {
-      id: USER.id,
+      id: userId,
       role: "USER",
-      wallet: USER.wallet,
-      country: USER.country,
-      username: USER.username,
+      address: address32,
+      pubkeyhash,
+      country,
+      username,
     };
 
-    const token = createToken(userData);
+    console.log(schemeValue);
 
     req.session = {
-      jwt: token,
+      jwt: createToken(userData),
     };
+
+    await connection.execute(schemeData, schemeValue);
+
+    await connection.commit();
 
     res.status(200).send({ success: true, data: userData });
   } catch (err) {
     await connection.rollback();
-
     _.error(err);
-
-    throw new BadRequestError("Invalid credentials or check your wallet");
   } finally {
     connection.release();
   }
