a0f81397fff39ac12eeaf457430e3e1b0a57491a
Juan Rey <93360398+rey-sudo@users.noreply.github.com>
2025-02-18 22:47:25 -0500

    Update secrets.sh

--- secrets.sh
+++ secrets.sh
@@ -8,26 +8,19 @@ kubectl create secret generic encrypto-secret --from-literal=ENCRYPTO_KEY=passwo
 
 kubectl create secret generic db-password --from-literal=password=password
 
-kubectl create secret generic gemini-api-key --from-literal=key=AIzaSyAiDvIcCUFn0AzocolOHM17R8XBIoWgg-g
+kubectl create secret generic gemini-api-key --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic blockfrost-key --from-literal=key=preprod9lmS1dJGXuVUEjDU7pkmPCy0PIGZl5L6
+kubectl create secret generic blockfrost-key --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic kupo-key --from-literal=key=https://kupo142sza2fs2wddgdyvkn9.preprod-v2.kupo-m1.demeter.run
+kubectl create secret generic kupo-key --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic ogmios-key --from-literal=key=https://ogmios12taxsanyhhzryv28pme.preprod-v6.ogmios-m1.demeter.run
+kubectl create secret generic ogmios-key --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic matrix-api-key --from-literal=key=AIzaSyANhfEHf9U-mN-I_XWgPbJPw3bGTaRFOnY
+kubectl create secret generic matrix-api-key --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic elastic-endpoint --from-literal=key=https://7db14714b4ec4653bf289ba710f61183.us-central1.gcp.cloud.es.io:443
+kubectl create secret generic elastic-endpoint --from-literal=key=word-word-word-word-word
 
-kubectl create secret generic elastic-api-key --from-literal=key=Q2Y2aWZKUUJNdTdDcjNDN2NLbGs6SWtNQl8tSDdRdzZvVFJTNmpxcEYtZw==
+kubectl create secret generic elastic-api-key --from-literal=key=word-word-word-word-word
 
-#{
-#  "id": "Cf6ifJQBMu7Cr3C7cKlk",
-#  "name": "lovelacers",
-#  "api_key": "IkMB_-H7Qw6oTRS6jqpF-g",
-#  "encoded": "Q2Y2aWZKUUJNdTdDcjNDN2NLbGs6SWtNQl8tSDdRdzZvVFJTNmpxcEYtZw==",
-#  "beats_logstash_format": "Cf6ifJQBMu7Cr3C7cKlk:IkMB_-H7Qw6oTRS6jqpF-g"
-#}
 
 
